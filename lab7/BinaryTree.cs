dc51a781afa42508d95d8cc5721c842c6ff2901c
user3124 <username040424@gmail.com>
2025-04-30 09:56:41 +0700

    Добавил класс BinaryTree

---
+++ lab7/BinaryTree.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    public class BinaryTree<T> where T : IComparable<T>
+    {
+        private BinaryTreeNode<T> _root; // Корень дерева
+        private BinaryTreeNode<T> _currentNode; // Текущий узел
+
+        // Конструктор
+        public BinaryTree()
+        {
+            _root = null;
+            _currentNode = null;
+        }
+    }
+}
