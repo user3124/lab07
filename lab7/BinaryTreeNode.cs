6c898297fbcae6fd2eeb3825c17aa9daa2528c24
user3124 <username040424@gmail.com>
2025-04-30 09:53:28 +0700

    Добавил новые файлы проекта

---
+++ lab7/BinaryTreeNode.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab7
+{
+    public class BinaryTreeNode<T>
+    {
+        public T Data { get; set; }
+        public BinaryTreeNode<T> Left { get; set; }
+        public BinaryTreeNode<T> Right { get; set; }
+        public BinaryTreeNode<T> Parent { get; set; }
+    }
+}
